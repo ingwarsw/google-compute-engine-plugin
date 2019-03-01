c0eb2e9d342b4a08aaeef8b2a5d055b00ee5072b
Karol Lassak <ingwar@ingwar.eu.org>
2019-03-01 14:54:47 +0100

    Revert "ci: add temporary pipeline support"
    This reverts commit bfacd35a03a99052af21745a779bb2a6a55289c7.

--- ci/install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-fly -t jenkins set-pipeline \
-  -c pipeline.yml \
-  -v project_id=$PROJECT_ID \
-  -v service_account_json=$SERVICE_ACCOUNT_JSON \
-  -p compute-engine-plugin-develop
--- ci/tasks/test-unit.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -e -u -x
-
-pushd plugin
-  mvn test -e
-popd
